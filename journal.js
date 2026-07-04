dc5ec0c19cb6f0d530db038ecfb5094dd61f995b
Noah Whitesides <whitesides_noah_mi01@student.smc.edu>
2026-07-03 18:38:16 -0700

    Five unique JS functions

--- journal.js
+++ journal.js
@@ -1,3 +1,22 @@
 function greet(name) {
     return "Hello, " + name + "!";
 }
+
+function evenCheck(numToCheck) {
+    if (numToCheck % 2 === 0) {
+        return numToCheck + " is even.";
+    } else {
+        return numToCheck + " is not even.";
+    }
+}
+function squareNumber(numToSquare) {
+    return numToSquare * numToSquare;
+}
+function farenToC(faren) {
+    return (faren - 32) * 5 / 9;
+    }
+function milesTraveled(mph, hours){
+    return "You travelled " + (mph * hours) + " miles"
+}
+
+
